ed5d78b9a978cc2f932da906d60cfbd5db029d9a
Valentin Damyanov <valentin.damyanov@ampeco.global>
2024-05-30 09:56:52 +0300

    Refactor TransactionResultResponse for additional data check

--- src/Message/TransactionResultResponse.php
+++ src/Message/TransactionResultResponse.php
@@ -2,32 +2,32 @@
 
 namespace Ampeco\OmnipayOtpGroup\Message;
 
-
 class TransactionResultResponse extends Response implements ProvidesCardInfo
 {
-
     /**
      * @return bool
      */
     public function isSuccessful(): bool
     {
-        return parent::isSuccessful() && array_key_exists('orderStatus', $this->data)
-            && (int)$this->data['orderStatus'] < 3; // 0-pending; 1-authorized; 2-paid
-
+        return parent::isSuccessful()
+            && array_key_exists('orderStatus', $this->data)
+            && (int) $this->data['orderStatus'] < 3 // 0-pending; 1-authorized; 2-paid
+            && array_key_exists('bindingInfo', $this->data);
     }
 
     public function isPending()
     {
-        return (int)$this->data['orderStatus'] === 0;
+        return (int) $this->data['orderStatus'] === 0;
     }
 
     public function getTransactionReference()
     {
-        foreach ((array)@$this->data['attributes'] as $attribute) {
+        foreach ((array) @$this->data['attributes'] as $attribute) {
             if ($attribute['name'] === 'mdOrder') {
                 return $attribute['value'];
             }
         }
+
         return null;
     }
 
@@ -44,7 +44,7 @@ class TransactionResultResponse extends Response implements ProvidesCardInfo
      */
     public function getExpireMonth()
     {
-        return (int)substr($this->data['cardAuthInfo']['expiration'], -2);
+        return (int) substr($this->data['cardAuthInfo']['expiration'], -2);
     }
 
     /**
@@ -52,7 +52,7 @@ class TransactionResultResponse extends Response implements ProvidesCardInfo
      */
     public function getExpireYear()
     {
-        return (int)substr($this->data['cardAuthInfo']['expiration'], 0, 4);
+        return (int) substr($this->data['cardAuthInfo']['expiration'], 0, 4);
     }
 
     /**
@@ -65,7 +65,7 @@ class TransactionResultResponse extends Response implements ProvidesCardInfo
 
     public function getToken()
     {
-        return $this->data['bindingInfo']['bindingId'];
+        return $this->data['bindingInfo']['bindingId'] ?? null;
     }
 
     public function getCardReference()
